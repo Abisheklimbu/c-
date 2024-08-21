3b71a6e1d229306beff7ae602ec091abc24c5197
Abishek <limbuabishek802@gmail.com>
2024-08-21 16:58:44 +0545

    New File

---
+++ First_cpp/first.cpp
@@ -0,0 +1,34 @@
+// // // Adding two numbers
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int a,b,c;
+    
+//     cout<<"Enter two number ";
+//     cin>>a>>b;
+
+//     c=a+b;
+
+//     cout<<"Addition of "<<a <<" + "<<b<<" is "<<c<<endl;
+
+//     return 0;
+// }
+
+
+// Reading user name and diplaying welcome message 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+
+    cout<<" What is your name ? ";
+    getline(cin, name);
+
+    cout<<"Welocme "<<name<<endl;
+
+    return 0;
+}
